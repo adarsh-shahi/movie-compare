10d2744f4553a0837fac03c82b9117707d858a5f
adarsh-shahi <adarshshahi1009@gmail.com>
2023-01-02 21:33:35 +0530

    omdb api testing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(`hey there`);
+const fetchData = async () =>{
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: "a60862d6",
+      t: "the batman"
+    }
+  })
+  console.log(response.data);
+}
+
+fetchData()
